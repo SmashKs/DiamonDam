939bd75661b6c0833a8c9e28694f961a37283955
Jieyi <jieyi.wu@oyolife.co.jp>
2019-08-26 19:11:06 +0900

    Modify: modified the docker compose file setting.

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask_restplus import Resource
-
-from rest_api.apis import api
-
-
-@api.route('/<string:id>')
-class HelloWorld(Resource):
-    def get(self, id):
-        return {id: 'world'}
--- diamondam.py
+++ diamondam.py
@@ -1,4 +1,12 @@
-from rest_api import create_app
+from flask_restplus import Resource
+
+from rest_api import api, create_app
 from rest_api.config import DevConfig
 
 app = create_app(DevConfig)
+
+
+@api.route('/<string:id>')
+class HelloWorld(Resource):
+    def get(self, id):
+        return {id: 'world'}
