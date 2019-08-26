e2382a4da3df130cea566be77a1859de6b1e460b
Jieyi <pokkbaby@gmail.com>
2019-08-26 18:35:36 +0900

    Change: changed the setting file.

--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -3,7 +3,7 @@ from flask import Config, Flask
 from rest_api.apis import api
 
 
-def create_app(conf: Config):  # type: (Config) -> Flask
+def create_app(conf: Config) -> Flask:
     app = Flask(__name__)
     app.config.from_object(conf)
     api.init_app(app)
